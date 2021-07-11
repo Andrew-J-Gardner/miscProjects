e0132bcdc2e703099c8cb7eddc5b4bf528a0553f
BuildTools <unconfigured@null.spigotmc.org>
2021-07-11 15:58:44 -0500

    Fixed all errors and tested functionality of groceryPath functions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,10 +17,10 @@ private:
     int aisleLocated;
     bool isPerishable;
 public:
-    groceryItem(string p_name, int p_aisleLocated, bool p_isPerishable = false){
-        name = p_name;
-        aisleLocated = p_aisleLocated;
-        isPerishable = p_isPerishable;
+    groceryItem(string p_name = "NuLL", int p_aisleLocated = -1, bool p_isPerishable = false){
+        this->name = p_name;
+        this->aisleLocated = p_aisleLocated;
+        this->isPerishable = p_isPerishable;
     }
     string getName() {   return name;    }
     int getAisle() {   return aisleLocated;    }
@@ -32,27 +33,36 @@ private:
     const int TRAVEL_TIME_BTWN_ISLES = 3;
 public:
     groceryPath(){
-
+        //listOfItems.resize(1);
     }
+    int getListLength(){    return this->listOfItems.size(); }
     void addItemToList(groceryItem p_item){
-        if(p_item.getName != NULL){
-            this.listOfItems.push_back(p_item);
+        if(p_item.getName() != " "){
+            this->listOfItems.push_back(p_item);
         }
-        else cout << "Error in adding item: item has no name!"
+        else cout << "Error in adding item: item has no name!" <<endl;
+    }
+    
+    void removeItemFromList(int p_itemIndex){
+        if(this->listOfItems.size() > p_itemIndex){
+            this->listOfItems.erase(listOfItems.begin() + (p_itemIndex-1));
+        } else { cout << "No such item exists!" << endl; }
     }
     void removeItemFromList(groceryItem p_item){
-        if(listOfItems.findItemInList(p_item.getName()))
+        int searchInd = findItemInList(p_item);
+        removeItemFromList(searchInd);
     }
+
     //If an element with the string name provided exists in the listOfItems, finds it.
     int findItemInList(string p_searchStr){
         string searchStr = p_searchStr;
-        for(int i=0; i<listOfItems.length; i++){
-            if(listOfItems.at(i).getName() == searchStr){
-                cout << "Item with name " << seachStr << " found at index (" << i << ")" << endl;
+        for(int i=0; i<this->listOfItems.size(); i++){
+            if(this->listOfItems.at(i).getName() == searchStr){
+                cout << "Item with name " << searchStr << " found at index (" << i << ")" << endl;
                 return i;
             }
         }
-        cout << "Item with name " << seachStr << " NOT found" << endl;
+        cout << "Item with name " << searchStr << " NOT found" << endl;
         return -1;
     }
     //If an element with the string name from the item provided exists in the listOfItems, finds it.
@@ -60,12 +70,9 @@ public:
         return findItemInList(p_item.getName());
     }
 
-    void removeItemFromList(int p_itemIndex){
-        if(listOfItems.at(p_itemIndex) != NULL){
-            listOfItems.erase(p_itemIndex);
-        } else { cout << "No such item exists!" }
+    groceryItem getItemAt(int p_index){
+        return this->listOfItems.at(p_index);
     }
-
     void generatePath(){
 
 
@@ -79,6 +86,9 @@ public:
 
 int main(){
     groceryPath myPath;
-    groceryItem bread("bread", 3);
+    groceryItem bread("bread", 3, false);
+    cout <<"first items name is: " << bread.getName() << endl;
     myPath.addItemToList(bread);
+    cout <<"name of first item is: " << myPath.getItemAt(0).getName() <<endl;
+    cout <<"length of list: " << myPath.getListLength() <<endl;
 }
