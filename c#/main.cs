70fa193dc901acf961be96769acb1f0a09bf4011
Andrew-J-Gardner <andrew@texasgardners.com>
2021-07-21 11:48:22 -0500

    Added C# folder and main file placeholder

---
+++ c#/main.cs
@@ -0,0 +1,19 @@
+using System;
+
+//cSharpProgramAlpha
+namespace CSPA {
+    class alphaMain {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello world!");
+
+            //Keeps from terminating early
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+
+}
